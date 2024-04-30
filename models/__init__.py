5d1c18beac17a80c67b11950c9cc7d0408f9fc50
Moctar K. NAMADOU <kamamoctar@gmail.com>
2024-04-30 23:27:35 +0000

    melanger a l'ancien CD

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-"""
-This module initializes the file storage system by creating an instance of FileStorage
-and reloading any existing data from the file system into the application.
-"""
-
+"""__init__ magic method for models directory"""
 from models.engine.file_storage import FileStorage
 
-# Create an instance of FileStorage to handle data persistence.
-storage = FileStorage()
 
-# Load existing data from storage into the application.
+storage = FileStorage()
 storage.reload()
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,22 +6,6 @@ Unittest classes:
     TestBaseModel_save
     TestBaseModel_to_dict
 """
-
-from models.base_model import BaseModel
-import unittest
-import os
-import models
-from datetime import datetime
-from time import sleep
-
-#!/usr/bin/python3
-"""Defines unittests for models/base_model.py.
-
-Unittest classes:
-    TestBaseModel_instantiation
-    TestBaseModel_save
-    TestBaseModel_to_dict
-"""
 import os
 import models
 import unittest
